c8553695ab3e2f741e4c700bd0f8a735134a0a9a
Amarjeet <amarjeet.prajapati@unthinkable.co>
2025-08-29 13:58:01 +0530

    udated config

--- playwright.config.js
+++ playwright.config.js
@@ -75,7 +75,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL, // ✅ Use env variable
-    headless: false,
+    // headless: false,
+    headless: true,
     // screenshot: "only-on-failure",
     screenshot: "on",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
